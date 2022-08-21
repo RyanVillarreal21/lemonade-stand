dc6eceb6633155fdb2fc607de99ec671feadc419
Ryan Villarreal <ryan.villarreal24@yahoo.com>
2022-08-20 19:14:47 -0500

    Created react-app

---
+++ src/components/ProductsList.js
@@ -0,0 +1,32 @@
+const ProductList = () => {
+  return (
+    <div>
+      <div>
+        <p>Lemons</p>
+        <div>
+          <button>+</button>
+          <h4>0</h4>
+          <button>-</button>
+        </div>
+      </div>
+      <div>
+        <p>Sugar</p>
+        <div>
+          <button>+</button>
+          <h4>0</h4>
+          <button>-</button>
+        </div>
+      </div>
+      <div>
+        <p>Ice Cubes</p>
+        <div>
+          <button>+</button>
+          <h4>0</h4>
+          <button>-</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
